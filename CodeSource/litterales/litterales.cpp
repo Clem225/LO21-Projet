56bdac11622878d0f0da8d50b0722264646bc551
Clem225 <clem225@gmail.com>
2016-05-23 17:52:08 +0200

    Ajout de l'opérateur '+' dans Entier
    Opérateur '+' avec complexe pas terminé (presque cela dit)

--- CodeSource/litterales/litterales.cpp
+++ CodeSource/litterales/litterales.cpp
@@ -1,5 +1,10 @@
 #include "litterales.h"
 
+
+
+
+/*------Classe Entier------*/
+
 Entier& Entier::operator=(Entier a)
 {
     // On verifie que ce n'est pas un cas a=a;
@@ -9,6 +14,103 @@ Entier& Entier::operator=(Entier a)
     }
     return *this;
 }
+
+/*------Opérateurs------*/
+
+Litterale& Entier::operator+(Litterale& e) {
+    Entier* ent=dynamic_cast<Entier*>(&e);
+    Rationnel* rat=dynamic_cast<Rationnel*>(&e);
+    Reel* real=dynamic_cast<Reel*>(&e);
+    Complexe* comp=dynamic_cast<Complexe*>(&e);
+
+    if (ent) //si le cast a réussi, c'est à dire si e est bien un entier
+    {
+        nb+=ent->getNb();
+        return *this;
+    }
+    if (rat) //c'est un rationnel
+    {
+        rat->setNum(rat->getNum() + nb*rat->getDenom());
+        rat->simplification();
+        return *rat;
+    }
+    if (real) //c'est un reel
+    {
+        real->setEntiere(real->getEntiere() + nb);
+        return *real;
+    }
+    if (comp) //c'est un complexe
+    {
+        //On cherche le type de la partie réelle
+        Entier* compEnt=dynamic_cast<Entier*>(comp->getReal());
+        Rationnel* compRat=dynamic_cast<Rationnel*>(comp->getReal());
+        Reel* compReal=dynamic_cast<Reel*>(comp->getReal());
+
+        if (compEnt) //La partie reelle du complexe est un entier
+        {
+            comp->setReal(Entier(compEnt->getNb() + nb));
+            return *comp;
+        }
+        if (compRat) //C'est un rationnel
+        {
+            compRat->setNum(compRat->getNum() + nb*compRat->getDenom());
+            compRat->simplification();
+            comp->setReal(*compRat);
+            return *comp;
+        }
+
+
+
+    }
+        
+
+}
+
+
+
+
+
+
+/*------Classe rationnel------*/
+
+Entier Rationnel::simplification() {
+    /* utilisation de l’algorithme d’Euclide pour trouver le Plus Grand Commun
+    Denominateur (PGCD) entre le num et le denominateur */
+    int num=getNum(), denom=getDenom();
+    // on ne travaille qu’avec des valeurs positives...
+    if (num<0) num=-num; if (denom<0) denom=-denom;
+    while(num!=denom){ if (num>denom) num=num-denom; else denom=denom-num; }
+    // on divise le num et le denom par le PGCD=a
+    setNum(getNum()/num); setDenom(getDenom()/num);
+    // si le denom est négatif, on fait passer le signe - au denom
+    if (getDenom()<0) { setDenom(-getDenom()); setNum(-getNum()); }
+
+    //Test pour tranformation en Entier. On ne retourne une valeur que si c'est tranformable en entier
+    if ((numerateur.getNb()==0) || (denominateur.getNb()==1))
+        return Entier(numerateur.getNb());
+}
+
+
+
+Rationnel::Rationnel(int e1, int e2) {
+    if (e2==0) throw "Erreur, division par zero";
+    numerateur=e1;
+    denominateur=e2;
+    simplification();
+}
+
+
+
+
+
+
+
+
+
+
+
+/*------Classe Réel------*/
+
 
 
 Reel::Reel(int& e1, int& e2) {
@@ -25,26 +127,6 @@ double Reel::getNb() const {
         return entiere.getNb()+temp;
     else
         return entiere.getNb()-temp;
-}
-
-template<typename Type> Type operator/(Entier a, Entier b)
-{
-    // si le numerateur est 0, le denominateur prend la valeur 1
-    if (a.getNb()==0) b.setValue(1);
-    if (b.getNb()==0) throw "Erreur, division par zero";
-    /* utilisation de l’algorithme d’Euclide pour trouver le Plus Grand Commun
-    Denominateur (PGCD) entre le numerateur et le denominateur */
-    int tempa =a.getNb(), tempb=b.getNb();
-    // on ne travaille qu’avec des valeurs positives...
-    if (tempa<0) tempa=-tempa; if (tempb<0) tempb=-tempb;
-    while(tempa!=tempb){ if (tempa>tempb) tempa=tempa-tempb; else tempb=tempb-tempa; }
-    // on divise le numerateur et le denominateur par le PGCD=a
-    a.setValue(a.getNb()/tempa); b.setValue(b.getNb()/tempa);
-    // si le denominateur est négatif, on fait passer le signe - au denominateur
-    if (b.getNb()<0) { b.setValue(-b.getNb()); a.setValue(-a.getNb()); }
-
-    if (b==1) {delete b; return a;}
-    else return Rationnel(a,b);
 }
 
 
--- CodeSource/litterales/litterales.h
+++ CodeSource/litterales/litterales.h
@@ -6,25 +6,32 @@
  * \version 0.1
  */
 
+#ifndef LITTERALES_H
+#define LITTERALES_H
+
 #include <iostream>
+
+
+
+
+
+/*----------------------Classes mères-------------------------*/
+
+
+class Operande {
+
+}; //Utilité à vérifier
+
 
 /*!
  * \class Litterale
 */
 
-
-class Operande {
-
-};
-
-class Operateur : public Operande {
-
-};
-
 class Litterale : public Operande {
 public:
     virtual void afficher(std::ostream& f) const=0;
 };
+
 
 class Programme : public Litterale {
 private:
@@ -52,6 +59,7 @@ private:
  * \class LitteraleNumerique
 */
 class LitteraleNumerique : public ExpressionPart {
+
 };
 
 /*!
@@ -63,9 +71,18 @@ private:
 public:
     Atome(char* text): tab(text) {}
     char* getAtome() const {return tab;}
+
+
+
+
 };
 
 
+
+
+
+
+/*----------------------Classes de littérales numériques-------------------------*/
 
 
 
@@ -84,6 +101,14 @@ public:
     void setValue(int i) {nb=i;}
     void afficher(std::ostream& f) const {f<<nb;}
 
+    /*----------Opérateurs-------------*/
+
+    Litterale& operator+(Litterale& e);
+    Entier& operator-(Entier& e) {nb-=e.getNb(); return *this;}
+    Entier& operator*(Entier& e) {nb*=e.getNb(); return *this;}
+
+
+
 };
 
 
@@ -95,10 +120,12 @@ private:
 	Entier numerateur;
 	Entier denominateur;
 public:
-    // Constructeur (Avec conversion implicite de int e1,e2 en Entier
-    Rationnel(int e1, int e2): numerateur(e1), denominateur(e2) {};
+    Entier simplification();
+    Rationnel(int e1, int e2);
 	int getNum() const {return numerateur.getNb();}
 	int getDenom() const {return denominateur.getNb();}
+    void setNum(int e) {numerateur=e;}
+    void setDenom(int e) {denominateur=e;}
     double getNb() const {return numerateur.getNb()/denominateur.getNb();} //Utilité à démontrer
     void afficher(std::ostream& f) const {f<<getNum()<<"/"<<getDenom();}
 };
@@ -116,8 +143,10 @@ public:
     Reel(int& e1, int& e2);
     Reel NEG() {if (entiere.getNb()>0) entiere=entiere.NEG(); return *this;}
     double getNb() const;
-    int getEntiere() const {return entiere.getNb();} //Utilité à démontrer
-    int getMantisse() const {return mantisse.getNb();} //Utilité à démontrer
+    int getEntiere() const {return entiere.getNb();}
+    int getMantisse() const {return mantisse.getNb();}
+    void setEntiere(int e) {entiere=e;}
+    void setMantisse(int e) {mantisse=e;}
     void afficher(std::ostream& f) const {f<<getNb();}
 
 };
@@ -132,14 +161,13 @@ private:
     LitteraleNumerique* imagPart;
 public:
     Complexe(LitteraleNumerique& real, LitteraleNumerique& imag): realPart(&real), imagPart(&imag) {}
-    LitteraleNumerique& getReal() const {return *realPart;}
-    LitteraleNumerique& getImag() const {return *imagPart;}
-    void afficher(std::ostream& f) const {getReal().afficher(f); f<<"$"; getImag().afficher(f);} // A vérif
+    LitteraleNumerique* getReal() const {return realPart;}
+    LitteraleNumerique* getImag() const {return imagPart;}
+    void setReal(LitteraleNumerique& lit) {realPart=&lit;}
+    void setImag(LitteraleNumerique& lit) {imagPart=&lit;}
+    void afficher(std::ostream& f) const {getReal()->afficher(f); f<<"$"; getImag()->afficher(f);} // A vérif
 };
 
 
 
-
-
-
-template<typename Type> Type operator/(Entier e1, Entier e2); // A vérifier par la suite
+#endif
