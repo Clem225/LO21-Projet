205f9e6007be0bd2be23762689296775700e2e84
Clem225 <clem225@gmail.com>
2016-05-26 12:33:15 +0200

    Opérateurs de base sur Entier terminés
    +, -, * et / fonctionnels sur Entier.

--- CodeSource/main/main.cpp
+++ CodeSource/main/main.cpp
@@ -10,11 +10,13 @@
 
 int main(int argc, char *argv[])
 {
+    Entier monEntier(4);
+    Entier monEntier2(8);
 
     Reel monReel(2);
 
-    Reel monReelpart1(2);
-    Reel monReelpart2(4);
+    Reel monReelpart1(2.0);
+    Reel monReelpart2(4.0);
 
     Reel monReelpart3(1.24);
     Reel monReelpart4(7.9);
@@ -22,12 +24,13 @@ int main(int argc, char *argv[])
     Complexe monComplexe1(monReelpart1,monReelpart2);
     Complexe monComplexe2(monReelpart3,monReelpart4);
 
-    Entier monEntier(4);
-    Entier monEntier2(8);
+
+
+    Complexe monComplexe3(monEntier,monEntier2);
 
     Rationnel monRat(3,4);
 
-    (monEntier/monRat).afficher();
+    (monEntier/monComplexe2).afficher();
 
     std::cout<<std::endl;
 
